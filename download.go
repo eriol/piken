7cbd7195756bd5af46389d6044f02dd98e6cbe59
Daniele Tricoli <eriol@mornie.org>
2015-09-05 03:02:47 +0200

    Download files from HTTP

---
+++ download.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func download(url, output string) error {
+
+	r, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	out, err := os.Create(output)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// io.copyBuffer, the actual implementation of io.Copy, reads maximum 32 KB
+	// from input, writes to output and then repeats. No need to worry about
+	// the size of file to download.
+	_, err = io.Copy(out, r.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
